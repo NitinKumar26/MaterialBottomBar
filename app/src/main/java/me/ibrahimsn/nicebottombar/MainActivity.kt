4d6df39cf6ab0c04225ff42745f06b196abc737a
Nitin Kumar <nk037578@gmail.com>
2019-03-20 22:43:35 +0530

    First commit

---
+++ app/src/main/java/me/ibrahimsn/nicebottombar/MainActivity.kt
@@ -0,0 +1,26 @@
+package me.ibrahimsn.nicebottombar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import me.ibrahimsn.lib.NiceBottomBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomBar.setActiveItem(1)
+
+        bottomBar.setBottomBarCallback(object: NiceBottomBar.BottomBarCallback {
+            override fun onItemSelect(pos: Int) {
+
+            }
+
+            override fun onItemReselect(pos: Int) {
+
+            }
+        })
+    }
+}
